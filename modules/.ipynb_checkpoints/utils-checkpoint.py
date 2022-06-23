f0c454bbde4999e967d41cba51dab8acf3e44bbc
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2022-06-23 15:49:10 +0200

    small changes

--- modules/.ipynb_checkpoints/utils-checkpoint.py
+++ modules/.ipynb_checkpoints/utils-checkpoint.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import multivariate_normal
 from scipy.optimize import fsolve
 from scipy.linalg import eigh
 from tqdm.auto import tqdm, trange
@@ -23,6 +25,93 @@ def _make_ellipse(mean, cov, ax, level=0.95, color=None, label = None, ls = '-',
         if n==1:
             ax.plot([],[],ls,color = color, label = label)
         ax.add_artist(ell)
+
+def corner_plot_Fisher(cov_param, mean, label_param, ax, color = None, set_axis = False):
+   
+    r"""
+    Attributes:
+    -----------
+    cov_param: array
+        parameter covarance of the n parameters
+    mean: array
+        means of parameters
+    label_param: list
+        name of parameters
+    ax: fig
+        ax
+    color: str
+        color of contours
+    set_axis: Bool
+        set axis or not
+    """
+    plt.subplots_adjust(left=0.125,
+                            bottom=0.1, 
+                            right=0.9, 
+                            top=0.9, 
+                            wspace=0.1, 
+                            hspace=0.1)
+    n_param = len(mean)
+    
+    #plot ellipse and gaussian single variate distribution
+    for j in range(n_param):
+        for i in range(n_param):
+            if i == j: 
+                x = np.linspace(-10, 100, 3000)
+                ax[i,i].plot(x, multivariate_normal.pdf(x, mean=mean[i], cov=cov_param[i,i]), color)
+                continue
+
+            if j > i: continue
+            index_i = i
+            index_j = j
+            cov_param_ij = np.zeros([2,2])
+            cov_param_ij[0,0] = cov_param[j,j]
+            cov_param_ij[1,1] = cov_param[i,i]
+            cov_param_ij[1,0] = cov_param[i,j]
+            cov_param_ij[0,1] = cov_param_ij[1,0]
+            mean_ij = [mean[j], mean[i]]
+            _make_ellipse(mean_ij, cov_param_ij, ax[i,j], level=0.95, color='k', label = None, ls = '-', facecolor = color, alpha=.1)
+            
+            
+    if set_axis == True:
+        #set xlim and ylim
+        n = 3
+        for i in range(n_param):
+            for j in range(i+1):
+            
+                mean_i = mean[i]
+                sigma_i = cov_param[i,i]**.5
+                mean_j = mean[j]
+                sigma_j = cov_param[j,j]**.5
+                if j < i: 
+                    ax[i,j].set_ylim(mean_i-n*sigma_i, mean_i+n*sigma_i)
+                    ax[i,j].set_xlim(mean_j-n*sigma_j, mean_j+n*sigma_j)
+                if j == i: 
+                    ax[i,j].set_xlim(mean_j-n*sigma_j, mean_j+n*sigma_j)
+                    ax[i,j].set_ylim(0)
+                    
+        # remove x and y ticks
+        ax[0,0].set_yticks([])
+        ax[0,0].set_xticks([])
+        for i in range(n_param):
+            for j in range(n_param):
+                ax[i,j].tick_params(axis='both', which = 'major', labelsize= 10)
+                if j != 0: 
+                    ax[i,j].set_yticks([])
+                if (i > 0)*(i < n_param-1):
+                     ax[i,j].set_xticks([])
+        for i in range(n_param):
+            for j in range(i+1,n_param):
+                ax[i,j].axis('off')
+                
+            
+        #set labl
+        for j in range(n_param):
+            ax[n_param-1, j].set_xlabel(label_param[j])
+        for i in range(1, n_param):
+            ax[i, 0].set_ylabel(label_param[i])   
+        
+                    
+        return None
 
 def _map_f(args):
     f, i = args
---
+++ modules/pinocchio_analysis/.ipynb_checkpoints/job_compute_model_per_block-checkpoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# SLURM options:
+
+# SBATCH --job-name=computemodel    # Job name
+# SBATCH --output=log_computemodel.log
+# SBATCH --partition=htc   # Partition choice
+# SBATCH --ntasks=1                    # Run a single task (by default tasks == CPU)
+# SBATCH --mem=4000                    # Memory in MB per default
+# SBATCH --time=0-01:00:00             # 7 days by default on htc partition
+# SBATCH --array=1-50
+
+N_JOB=$SLURM_ARRAY_TASK_MAX
+N_POINTS=10000
+N_PER_JOB=$(($N_POINTS / $N_JOB))
+N_Z_BIN=100
+N_M_BIN=100
+START=0
+START_NUM=$(($START+($SLURM_ARRAY_TASK_ID-1) * $N_PER_JOB ))
+END_NUM=$(($START+($SLURM_ARRAY_TASK_ID) * $N_PER_JOB-1))
+
+# Print the task and run range
+echo This is task $SLURM_ARRAY_TASK_ID, which will do runs from $START_NUM to $END_NUM
+
+source /pbs/home/c/cpayerne/setup_mydesc.sh
+python /pbs/throng/lsst/users/cpayerne/ClusterLikelihoods/modules/pinocchio_analysis/compute_model.py $N_Z_BIN $N_M_BIN $START_NUM $END_NUM
