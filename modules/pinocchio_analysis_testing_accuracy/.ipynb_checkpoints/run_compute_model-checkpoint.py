7868cdfbb206bd0055f0ff8d5597e698492e6246
Constantin Payerne <61975432+payerne@users.noreply.github.com>
2025-11-29 22:29:31 +0100

    Delete modules/pinocchio_analysis_testing_accuracy/.ipynb_checkpoints directory

--- modules/pinocchio_analysis_testing_accuracy/.ipynb_checkpoints/run_compute_model-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import os
-for i in range(100):
-    down, up=int(i*100), int((i+1)*100-1)
-    print(down, up)
-    os.system(f'python /pbs/throng/lsst/users/cpayerne/ClusterLikelihoods/modules/pinocchio_analysis/compute_model.py 100 100 {down} {up}')
-    print('done')
-   # $ compute_model.py 100 100 $down $up
